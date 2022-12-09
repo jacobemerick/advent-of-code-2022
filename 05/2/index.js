cede3abead567b457df6275e6e2999e1d9fe30ce
Jacob Emerick <jacob.emerick@quantumrhino.com>
2022-12-09 12:49:40 -0700

    Day 5

---
+++ 05/2/index.js
@@ -0,0 +1,51 @@
+const events = require('events');
+const fs = require('fs');
+const readline = require('readline');
+
+(async function rearrangeCrates() {
+  let stackedCrates = [];
+
+  const reader = readline.createInterface({
+    input: fs.createReadStream('./input.txt'),
+    crlfDelay: Infinity,
+  });
+
+  let initialStack = false;
+  const tempHolder = [];
+  reader.on('line', (line) => {
+    if (!initialStack) {
+      if (line !== '') {
+        tempHolder.push(line);
+        return;
+      }
+
+      const crateStackCount = tempHolder.pop().split('   ').length;
+      for (let i = 0; i < crateStackCount; i++) {
+        stackedCrates.push([]);
+      }
+
+      tempHolder.forEach((level) => {
+        level.match(/.{3,4}/g).forEach((col, stack) => {
+          const matchCrate = col.match(/[A-Z]{1}/);
+          if (!matchCrate) return;
+
+          stackedCrates[stack].push(matchCrate.shift());
+        });
+      });
+
+      initialStack = true;
+      return;
+    }
+
+    const parsedCommand = line.match(/move (\d*) from (\d*) to (\d*)/);
+    const moveCount = Number(parsedCommand[1]);
+    const fromCol = Number(parsedCommand[2]) - 1;
+    const toCol = Number(parsedCommand[3]) - 1;
+
+    stackedCrates[toCol].splice(0, 0, ...stackedCrates[fromCol].splice(0, moveCount));
+  });
+
+  await events.once(reader, 'close');
+
+  console.log(`${stackedCrates.map((stack) => stack[0]).join('')}`);
+})();
